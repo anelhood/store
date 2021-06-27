b5c8d2136447c11e26d13eb3fcafae2b5a526744
angelhood <15836200695@163.com>
2021-06-27 16:47:12 +0800

    zuoye

---
+++ demo3.py
@@ -0,0 +1,234 @@
+#
+id1 = "1号"
+menu1 = "胡辣汤"
+price1 = 10
+Inventory1 = 500
+market1 = 10
+
+id2 = "2号"
+menu2 = "烩面"
+price2 = 15
+Inventory2 = 600
+market2 = 60
+
+id3 = "3号"
+menu3 = "花生糕"
+price3 = 10
+Inventory3 = 335
+market3 = 43
+
+id4 = "4号"
+menu4 = "道口烧鸡"
+price4 = 30
+Inventory4 = 855
+market4 = 63
+
+id5 = "5号"
+menu5 = "牡丹饼"
+price5 = 10
+Inventory5 = 632
+market5 = 63
+
+id6 = "6号"
+menu6 = "桐子鸡"
+price6 = 30
+Inventory6 = 562
+market6 = 120
+
+id7 = "7号"
+menu7 = "烩面"
+price7 = 15
+Inventory7 = 600
+market7 = 72
+
+id8 = "8号"
+menu8 = "胡辣汤"
+price8 = 10
+Inventory8 = 500
+market8 = 69
+
+id9 = "9号"
+menu9 = "烩面"
+price9 = 15
+Inventory9 = 600
+market9 = 35
+
+id10 = "10号"
+menu10 = "胡辣汤"
+price10 = 10
+Inventory10 = 500
+market10 = 140
+
+id11 = "11号"
+menu11 = "烩面"
+price11 = 15
+Inventory11 = 600
+market11 = 90
+
+id12 = "12号"
+menu12 = "道口烧鸡"
+price12 = 30
+Inventory12 = 855
+market12 = 24
+
+
+id13 = "13号"
+menu13 = "仲景香菇酱"
+price13 = 10
+Inventory13 = 632
+market13 = 45
+
+id14 = "14号"
+menu14 = "花生糕"
+price14 = 10
+Inventory14 = 335
+market14 = 25
+
+id15 = "15号"
+menu15 = "烩面"
+price15 = 15
+Inventory15 = 600
+market15 = 60
+
+id16 = "16号"
+menu16 = "仲景香菇酱"
+price16 = 10
+Inventory16 = 632
+market16 = 129
+
+id17 = "17号"
+menu17 = "胡辣汤"
+price17 = 10
+Inventory17 = 500
+market17 = 10
+
+id18 = "18号"
+menu18 = "花生糕"
+price18 = 10
+Inventory18 = 335
+market18 = 43
+
+id19 = "19号"
+menu19 = "仲景香菇酱"
+price19 = 10
+Inventory19 = 632
+market19 = 63
+
+id20 = "20号"
+menu20 = "烩面"
+price20 = 15
+Inventory20 = 600
+market20 = 60
+
+id21 = "21号"
+menu21 = "道口烧鸡"
+price21 = 30
+Inventory21 = 855
+market21 = 63
+
+id22 = "22号"
+menu22 = "花生糕"
+price22 = 10
+Inventory22 = 335
+market22 = 60
+
+id23 = "23号"
+menu23 = "仲景香菇酱"
+price23 = 10
+Inventory23 = 632
+market23 = 58
+
+id24 = "24号"
+menu24 = "烩面"
+price24 = 15
+Inventory24 = 600
+market24 = 140
+
+id25 = "25号"
+menu25 = "仲景香菇酱"
+price25 = 10
+Inventory25 = 632
+market25 = 48
+
+id26 = "26号"
+menu26 = "花生糕"
+price26 = 10
+Inventory26 = 335
+market26 = 43
+
+id27 = "27号"
+menu27 = "道口烧鸡"
+price27 = 30
+Inventory27 = 855
+market27 = 57
+
+id28 = "28号"
+menu28 = "胡辣汤"
+price28 = 10
+Inventory28 = 500
+market28 = 10
+
+id29 = "29号"
+menu29 = "仲景香菇酱"
+price29 = 10
+Inventory29 = 632
+market29 = 63
+
+id30 = "30号"
+menu30 = "花生糕"
+price30 = 10
+Inventory30 = 335
+market30 = 78
+
+print ("------- 日期---------商品名称-------价格------库存信息---------销售量/每日")
+print ("\t\t" ,id1, "\t\t" ,menu1, "\t\t",price1,"\t\t",Inventory1,"\t\t\t",market1)
+print ("\t\t" ,id2, "\t\t" ,menu2, "\t\t",price2,"\t\t",Inventory2,"\t\t\t",market2)
+print ("\t\t" ,id3, "\t\t" ,menu3, "\t\t",price3,"\t\t",Inventory3,"\t\t\t",market3)
+print ("\t\t" ,id4, "\t\t" ,menu4, "\t",price4,"\t\t",Inventory4,"\t\t\t",market4)
+print ("\t\t" ,id5, "\t\t" ,menu5, "\t\t",price5,"\t\t",Inventory5,"\t\t\t",market5)
+print ("\t\t" ,id6, "\t\t" ,menu6, "\t\t",price6,"\t\t",Inventory6,"\t\t\t",market6)
+print ("\t\t" ,id7, "\t\t" ,menu7, "\t\t",price7,"\t\t",Inventory7,"\t\t\t",market7)
+print ("\t\t" ,id8, "\t\t" ,menu8, "\t\t",price8,"\t\t",Inventory8,"\t\t\t",market8)
+print ("\t\t" ,id9, "\t\t" ,menu9, "\t\t",price9,"\t\t",Inventory9,"\t\t\t",market9)
+print ("\t\t" ,id10, "\t\t" ,menu10, "\t\t",price10,"\t\t",Inventory10,"\t\t\t",market10)
+print ("\t\t" ,id11, "\t\t" ,menu11, "\t\t",price11,"\t\t",Inventory11,"\t\t\t",market11)
+print ("\t\t" ,id12, "\t\t" ,menu12, "\t",price12,"\t\t",Inventory12,"\t\t\t",market12)
+print ("\t\t" ,id13, "\t\t" ,menu13, "\t",price13,"\t\t",Inventory13,"\t\t\t",market13)
+print ("\t\t" ,id14, "\t\t" ,menu14, "\t\t",price14,"\t\t",Inventory14,"\t\t\t",market14)
+print ("\t\t" ,id15, "\t\t" ,menu15, "\t\t",price15,"\t\t",Inventory15,"\t\t\t",market15)
+print ("\t\t" ,id16, "\t\t" ,menu16, "\t",price16,"\t\t",Inventory16,"\t\t\t",market16)
+print ("\t\t" ,id17, "\t\t" ,menu17, "\t\t",price17,"\t\t",Inventory17,"\t\t\t",market17)
+print ("\t\t" ,id18, "\t\t" ,menu18, "\t\t",price18,"\t\t",Inventory18,"\t\t\t",market18)
+print ("\t\t" ,id19, "\t\t" ,menu19, "\t",price19,"\t\t",Inventory19,"\t\t\t",market19)
+print ("\t\t" ,id20, "\t\t" ,menu20, "\t\t",price20,"\t\t",Inventory20,"\t\t\t",market20)
+print ("\t\t" ,id21, "\t\t" ,menu21, "\t",price21,"\t\t",Inventory21,"\t\t\t",market21)
+print ("\t\t" ,id22, "\t\t" ,menu22, "\t\t",price22,"\t\t",Inventory22,"\t\t\t",market22)
+print ("\t\t" ,id23, "\t\t" ,menu23, "\t",price23,"\t\t",Inventory23,"\t\t\t",market23)
+print ("\t\t" ,id24, "\t\t" ,menu24, "\t\t",price24,"\t\t",Inventory24,"\t\t\t",market24)
+print ("\t\t" ,id25, "\t\t" ,menu25, "\t",price25,"\t\t",Inventory25,"\t\t\t",market25)
+print ("\t\t" ,id26, "\t\t" ,menu26, "\t\t",price26,"\t\t",Inventory26,"\t\t\t",market26)
+print ("\t\t" ,id27, "\t\t" ,menu27, "\t",price27,"\t\t",Inventory27,"\t\t\t",market27)
+print ("\t\t" ,id28, "\t\t" ,menu28, "\t\t",price28,"\t\t",Inventory28,"\t\t\t",market28)
+print ("\t\t" ,id29, "\t\t" ,menu29, "\t",price29,"\t\t",Inventory29,"\t\t\t",market29)
+print ("\t\t" ,id30, "\t\t" ,menu30, "\t\t",price30,"\t\t",Inventory30,"\t\t\t",market30)
+print("-------------------------------------------------------------------------------------")
+print("$","胡辣汤",price1 * market1+price8 * market8+price10 * market10+price17 * market17+price28 * market28)
+print("$","烩面",price2 * market2+price7 * market7+price9 * market9+price11 * market11+price15 * market15+price20 * market20+price24 * market24)
+print("$","花生糕",price3 * market3+price14 * market14+price18 * market18+price22 * market22+price26 * market26+price30 * market30)
+print("$","道口烧鸡",price4 * market4+price12 * market12+price21 * market21+price27 * market27)
+print("$","仲景香菇酱",price5 * market5+price13 * market13+price16 * market16+price19 * market19+price23 * market23+price23 * market23 + price29 * market29)
+print("$","桐子鸡",price6 * market6)
+print("$","总金额",2390+7755+2920+6210+4790+3600)
+print ("$","胡辣汤总金额占比",(price1 * market1 + price8 * market8 + price10 * market10 + price17 * market17 + price28 * market28)/27665*100,"%")
+print ("$","烩面总金额占比",(price2 * market2+price7 * market7+price9 * market9+price11 * market11+price15 * market15+price20 * market20+price24 * market24)/27665*100,"%")
+print ("$","花生糕总金额占比",(price3 * market3+price14 * market14+price18 * market18+price22 * market22+price26 * market26+price30 * market30)/27665*100,"%")
+print ("$","道口烧鸡总金额占比",(price4 * market4+price12 * market12+price21 * market21+price27 * market27)/27665*100,"%")
+print ("$","仲景香菇酱总金额占比",(price5 * market5+price13 * market13+price16 * market16+price19 * market19+price23 * market23+price23 * market23 + price29 * market29)/27665*100,"%")
+print ("$","桐子鸡总金额占比",(price6 * market6)/27665*100,"%")
+
+
+
+
+
+
+
