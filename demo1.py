adbd411458c9d700da624e3d84a61ca4108ff8ce
angelhood <15836200695@163.com>
2021-06-27 16:08:51 +0800

    第一次作业

---
+++ demo1.py
@@ -0,0 +1,236 @@
+#
+id1 = "1号"
+lothing1 = "羽绒服"
+price1 = 253.6
+Inventory1 = 500
+market1 = 10
+
+id2 = "2号"
+lothing2 = "牛仔裤"
+price2 = 86.3
+Inventory2 = 600
+market2 = 60
+
+id3 = "3号"
+lothing3 = "风衣"
+price3 = 96.8
+Inventory3 = 335
+market3 = 43
+
+id4 = "4号"
+lothing4 = "皮草"
+price4 = 135.9
+Inventory4 = 855
+market4 = 63
+
+id5 = "5号"
+lothing5 = "T血"
+price5 = 65.8
+Inventory5 = 632
+market5 = 63
+
+id6 = "6号"
+lothing6 = "衬衫"
+price6 = 49.3
+Inventory6 = 562
+market6 = 120
+
+id7 = "7号"
+lothing7 = "牛仔裤"
+price7 = 86.3
+Inventory7 = 600
+market7 = 72
+
+id8 = "8号"
+lothing8 = "羽绒服"
+price8 = 253.6
+Inventory8 = 500
+market8 = 69
+
+id9 = "9号"
+lothing9 = "牛仔裤"
+price9 = 86.3
+Inventory9 = 600
+market9 = 35
+
+id10 = "10号"
+lothing10 = "羽绒服"
+price10 = 253.6
+Inventory10 = 500
+market10 = 140
+
+id11 = "11号"
+lothing11 = "牛仔裤"
+price11 = 86.3
+Inventory11 = 600
+market11 = 90
+
+id12 = "12号"
+lothing12 = "皮草"
+price12 = 135.9
+Inventory12 = 855
+market12 = 24
+
+
+id13 = "13号"
+lothing13 = "T血"
+price13 = 65.8
+Inventory13 = 632
+market13 = 45
+
+id14 = "14号"
+lothing14 = "风衣"
+price14 = 96.8
+Inventory14 = 335
+market14 = 25
+
+id15 = "15号"
+lothing15 = "牛仔裤"
+price15 = 86.3
+Inventory15 = 600
+market15 = 60
+
+id16 = "16号"
+lothing16 = "T血"
+price16 = 65.8
+Inventory16 = 632
+market16 = 129
+
+id17 = "17号"
+lothing17 = "羽绒服"
+price17 = 253.6
+Inventory17 = 500
+market17 = 10
+
+id18 = "18号"
+lothing18 = "风衣"
+price18 = 96.8
+Inventory18 = 335
+market18 = 43
+
+id19 = "19号"
+lothing19 = "T血"
+price19 = 65.8
+Inventory19 = 632
+market19 = 63
+
+id20 = "20号"
+lothing20 = "牛仔裤"
+price20 = 86.3
+Inventory20 = 600
+market20 = 60
+
+id21 = "21号"
+lothing21 = "皮草"
+price21 = 135.9
+Inventory21 = 855
+market21 = 63
+
+id22 = "22号"
+lothing22 = "风衣"
+price22 = 96.8
+Inventory22 = 335
+market22 = 60
+
+id23 = "23号"
+lothing23 = "T血"
+price23 = 65.8
+Inventory23 = 632
+market23 = 58
+
+id24 = "24号"
+lothing24 = "牛仔裤"
+price24 = 86.3
+Inventory24 = 600
+market24 = 140
+
+id25 = "25号"
+lothing25 = "T血"
+price25 = 65.8
+Inventory25 = 632
+market25 = 48
+
+id26 = "26号"
+lothing26 = "风衣"
+price26 = 96.8
+Inventory26 = 335
+market26 = 43
+
+id27 = "27号"
+lothing27 = "皮草"
+price27 = 135.9
+Inventory27 = 855
+market27 = 57
+
+id28 = "28号"
+lothing28 = "羽绒服"
+price28 = 253.6
+Inventory28 = 500
+market28 = 10
+
+id29 = "29号"
+lothing29 = "T血"
+price29 = 65.8
+Inventory29 = 632
+market29 = 63
+
+id30 = "30号"
+lothing30 = "风衣"
+price30 = 96.8
+Inventory30 = 335
+market30 = 78
+
+print ("------- 日期---------服装名称-------价格/件------库存信息---------销售量/每日")
+print ("\t\t" ,id1, "\t\t" ,lothing1, "\t\t",price1,"\t\t",Inventory1,"\t\t\t",market1)
+print ("\t\t" ,id2, "\t\t" ,lothing2, "\t\t",price2,"\t\t",Inventory2,"\t\t\t",market2)
+print ("\t\t" ,id3, "\t\t" ,lothing3, "\t\t",price3,"\t\t",Inventory3,"\t\t\t",market3)
+print ("\t\t" ,id4, "\t\t" ,lothing4, "\t\t",price4,"\t\t",Inventory4,"\t\t\t",market4)
+print ("\t\t" ,id5, "\t\t" ,lothing5, "\t\t",price5,"\t\t",Inventory5,"\t\t\t",market5)
+print ("\t\t" ,id6, "\t\t" ,lothing6, "\t\t",price6,"\t\t",Inventory6,"\t\t\t",market6)
+print ("\t\t" ,id7, "\t\t" ,lothing7, "\t\t",price7,"\t\t",Inventory7,"\t\t\t",market7)
+print ("\t\t" ,id8, "\t\t" ,lothing8, "\t\t",price8,"\t\t",Inventory8,"\t\t\t",market8)
+print ("\t\t" ,id9, "\t\t" ,lothing9, "\t\t",price9,"\t\t",Inventory9,"\t\t\t",market9)
+print ("\t\t" ,id10, "\t\t" ,lothing10, "\t\t",price10,"\t\t",Inventory10,"\t\t\t",market10)
+print ("\t\t" ,id11, "\t\t" ,lothing11, "\t\t",price11,"\t\t",Inventory11,"\t\t\t",market11)
+print ("\t\t" ,id12, "\t\t" ,lothing12, "\t\t",price12,"\t\t",Inventory12,"\t\t\t",market12)
+print ("\t\t" ,id13, "\t\t" ,lothing13, "\t\t",price13,"\t\t",Inventory13,"\t\t\t",market13)
+print ("\t\t" ,id14, "\t\t" ,lothing14, "\t\t",price14,"\t\t",Inventory14,"\t\t\t",market14)
+print ("\t\t" ,id15, "\t\t" ,lothing15, "\t\t",price15,"\t\t",Inventory15,"\t\t\t",market15)
+print ("\t\t" ,id16, "\t\t" ,lothing16, "\t\t",price16,"\t\t",Inventory16,"\t\t\t",market16)
+print ("\t\t" ,id17, "\t\t" ,lothing17, "\t\t",price17,"\t\t",Inventory17,"\t\t\t",market17)
+print ("\t\t" ,id18, "\t\t" ,lothing18, "\t\t",price18,"\t\t",Inventory18,"\t\t\t",market18)
+print ("\t\t" ,id19, "\t\t" ,lothing19, "\t\t",price19,"\t\t",Inventory19,"\t\t\t",market19)
+print ("\t\t" ,id20, "\t\t" ,lothing20, "\t\t",price20,"\t\t",Inventory20,"\t\t\t",market20)
+print ("\t\t" ,id21, "\t\t" ,lothing21, "\t\t",price21,"\t\t",Inventory21,"\t\t\t",market21)
+print ("\t\t" ,id22, "\t\t" ,lothing22, "\t\t",price22,"\t\t",Inventory22,"\t\t\t",market22)
+print ("\t\t" ,id23, "\t\t" ,lothing23, "\t\t",price23,"\t\t",Inventory23,"\t\t\t",market23)
+print ("\t\t" ,id24, "\t\t" ,lothing24, "\t\t",price24,"\t\t",Inventory24,"\t\t\t",market24)
+print ("\t\t" ,id25, "\t\t" ,lothing25, "\t\t",price25,"\t\t",Inventory25,"\t\t\t",market25)
+print ("\t\t" ,id26, "\t\t" ,lothing26, "\t\t",price26,"\t\t",Inventory26,"\t\t\t",market26)
+print ("\t\t" ,id27, "\t\t" ,lothing27, "\t\t",price27,"\t\t",Inventory27,"\t\t\t",market27)
+print ("\t\t" ,id28, "\t\t" ,lothing28, "\t\t",price28,"\t\t",Inventory28,"\t\t\t",market28)
+print ("\t\t" ,id29, "\t\t" ,lothing29, "\t\t",price29,"\t\t",Inventory29,"\t\t\t",market29)
+print ("\t\t" ,id30, "\t\t" ,lothing30, "\t\t",price30,"\t\t",Inventory30,"\t\t\t",market30)
+print("-------------------------------------------------------------------------------------")
+print("$","羽绒服",price1 * market1+price8 * market8+price10 * market10+price17 * market17+price28 * market28)
+print("$","牛仔裤",price2 * market2+price7 * market7+price9 * market9+price11 * market11+price15 * market15+price20 * market20+price24 * market24)
+print("$","风衣",price3 * market3+price14 * market14+price18 * market18+price22 * market22+price26 * market26+price30 * market30)
+print("$","皮草",price4 * market4+price12 * market12+price21 * market21+price27 * market27)
+print("$","T血",price5 * market5+price13 * market13+price16 * market16+price19 * market19+price23 * market23+price23 * market23 + price29 * market29)
+print("$","衬衫",price6 * market6)
+print("$","总金额",60610 + 44617++28265+28131+31518+5916)
+print ("$","羽绒服总金额占比",(price1 * market1 + price8 * market8 + price10 * market10 + price17 * market17 + price28 * market28)/199057*100,"%")
+print ("$","牛仔裤总金额占比",(price2 * market2+price7 * market7+price9 * market9+price11 * market11+price15 * market15+price20 * market20+price24 * market24)/199057*100,"%")
+print ("$","风衣总金额占比",(price3 * market3+price14 * market14+price18 * market18+price22 * market22+price26 * market26+price30 * market30)/199057*100,"%")
+print ("$","皮草总金额占比",(price4 * market4+price12 * market12+price21 * market21+price27 * market27)/199057*100,"%")
+print ("$","T血总金额占比",(price5 * market5+price13 * market13+price16 * market16+price19 * market19+price23 * market23+price23 * market23 + price29 * market29)/199057*100,"%")
+print ("$","衬衫总金额占比",(price6 * market6)/199057*100,"%")
+
+
+
+
+
+
+
+
+
